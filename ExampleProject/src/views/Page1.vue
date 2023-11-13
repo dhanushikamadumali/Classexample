4eb890a8b5a2590082514cb62ffda956ac33466b
dhanushika <dhanushika76@gmail.com>
2023-11-13 05:38:33 +0530

    updated

--- ExampleProject/src/views/Page1.vue
+++ ExampleProject/src/views/Page1.vue
@@ -3,6 +3,29 @@
         <h1>
             hello page 1
         </h1>
+        <v-container class="bg-red">
+            <v-row no-gutters>
+
+            <v-col cols="12" sm="4" md="4">
+                <p class="ma-2 pa-2">one of three column</p>
+            </v-col>
+            <v-col cols="12" sm="4" md="4">
+                <p class="ma-2 pa-2">one of three column </p>
+            </v-col>
+            <v-col  cols="12" sm="4" md="4">
+                <p class="ma-2 pa-2">one of three column</p>
+            </v-col>
+
+            <v-img
+            :width="300"
+            aspect-ratio="16/9"
+            cover
+            src="../assets/image/material.jpg">
+
+            </v-img>
+            </v-row>
+        </v-container>
+       
        <page2/>
     </div>
 </template>
